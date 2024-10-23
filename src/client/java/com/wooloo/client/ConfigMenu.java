e328f480a84d1a5593fb41f72e98933066f9f0c1
brendon-pa <bpaolin1@binghamton.edu>
2024-10-22 23:56:03 -0400

    make durability warning for crafted items

--- src/client/java/com/wooloo/client/ConfigMenu.java
+++ src/client/java/com/wooloo/client/ConfigMenu.java
@@ -16,7 +16,8 @@ public class ConfigMenu extends Screen {
 		super(Text.literal("[WOOLOOMOD] Config"));
 	}
 	public ButtonWidget button1;
-	public TextFieldWidget button2;
+	public ButtonWidget button2;
+	public ButtonWidget button3;
 	@Override
 	protected void init() {
 		Options instance = Options.getInstance();
@@ -36,5 +37,33 @@ public class ConfigMenu extends Screen {
 										.build();
 		//button2 = new TextFieldWidget((Text.literal("API Key: ")) button -> {});
 		addDrawableChild(button1);
+		button2 = ButtonWidget.builder(Text.literal("Auto TCC Wings: " + options.get("autowings").toString()), button -> {
+					if(options.get("autowings").toString().equals("true")) {
+						instance.setJsonValue("autowings", "false");
+						button2.setMessage(Text.literal("Auto TCC Wings: false"));
+					}
+					else {
+						instance.setJsonValue("autowings", "true");
+						button2.setMessage(Text.literal("Auto TCC Wings: true"));
+					}
+				})
+				.dimensions(width / 2 - 205, 20, 200, 20)
+				.tooltip(Tooltip.of(Text.literal("Gives you the location of the exit in TCC wings room")))
+				.build();
+		addDrawableChild(button2);
+		button3 = ButtonWidget.builder(Text.literal("Auto TCC Wings: " + options.get("durabilitywarning").toString()), button -> {
+					if(options.get("durabilitywarning").toString().equals("true")) {
+						instance.setJsonValue("durabilitywarning", "false");
+						button3.setMessage(Text.literal("Durability Warning: false"));
+					}
+					else {
+						instance.setJsonValue("durabilitywarning", "true");
+						button3.setMessage(Text.literal("Durability Warning: true"));
+					}
+				})
+				.dimensions(width / 2 - 205, 20, 200, 20)
+				.tooltip(Tooltip.of(Text.literal("Warns you about low durability crafted items")))
+				.build();
+		addDrawableChild(button3);
 	}
 }
--- src/client/java/com/wooloo/client/WoolooClient.java
+++ src/client/java/com/wooloo/client/WoolooClient.java
@@ -5,10 +5,17 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.collection.DefaultedList;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,11 +25,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class WoolooClient implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("wooloo");
 	public static final Options optionsInstance = Options.getInstance();
 	private List<UUID> uuidList;
+	private boolean durabilityWarning = true;
+	private int tickCounter = 0;
 	@Override
 	public void onInitializeClient() {
 		this.uuidList = new ArrayList<>();
@@ -57,28 +69,43 @@ public class WoolooClient implements ClientModInitializer {
 							return 1;
 						}
 				)));
-		/*ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("peek")
-				.then(ClientCommandManager.argument("player", StringArgumentType.string())
-				.executes(context -> {
-							String playerName = StringArgumentType.getString(context, "player");
-							MinecraftClient client = context.getSource().getClient();
-							PlayerEntity playerObj = client.world.getPlayers().stream()
-									.filter(player -> player.getName().getString().equalsIgnoreCase(playerName))
-									.findFirst()
-									.orElse(null);
-							if(playerObj == null) {
-								context.getSource().sendFeedback(Text.literal("Peek failed, " + playerName + " is not in your world."));
-								return 1;
-							}
-							context.getSource().sendFeedback(Text.literal("Peeking " + playerName));
-							client.send(() -> client.setScreen(new InventoryScreen(playerObj)));
-							playerObj.getInventory().armor.stream()
-									.filter(itemStack -> !itemStack.isEmpty())
-									.forEach(itemStack -> {
-										context.getSource().sendFeedback(Text.literal(itemStack.));
-									});
-							return 1;
-						}
-				))));*/
+		ClientTickEvents.END_WORLD_TICK.register(world -> {
+			MinecraftClient client = MinecraftClient.getInstance();
+			if(client != null) {
+				tickCounter++;
+				if(tickCounter >= 300) {
+					tickCounter = 0;
+					durabilityWarning = !durabilityWarning;
+				}
+				if(durabilityWarning) {
+					durabilityWarning = !durabilityWarning;
+					DefaultedList<ItemStack> mainInv = client.player.getInventory().main;
+					DefaultedList<ItemStack> armorInv = client.player.getInventory().armor;
+					Pattern pattern = Pattern.compile("(\\d+)/(\\d+) Durability");
+					Stream<ItemStack> invStream = Stream.concat(mainInv.stream(), armorInv.stream());
+					invStream.filter(itemStack -> !itemStack.isEmpty())
+							.filter(itemStack -> ifInTooltip(itemStack, "Crafted") && ifInTooltip(itemStack, "Durability"))
+							.forEach(itemStack -> {
+									List<Text> tooltip = itemStack.getTooltip(Item.TooltipContext.DEFAULT, MinecraftClient.getInstance().player, TooltipType.BASIC);
+									String lastLine = tooltip.getLast().getString();
+									Matcher matcher = pattern.matcher(lastLine);
+									if(matcher.find()) {
+										float curDurability = Integer.parseInt(matcher.group(1));
+										float maxDurability = Integer.parseInt(matcher.group(2));
+										if(curDurability/maxDurability <= .5) {
+											client.player.sendMessage(Text.literal(itemStack.getName().getString() + " is low durability (" + (int) curDurability + "/" + (int) maxDurability + ")")
+													.withColor(Formatting.RED.getColorValue()));
+										}
+									}
+								}
+							);
+				}
+			}
+		});
+	}
+	private boolean ifInTooltip(ItemStack itemStack, String searchString) {
+		List<Text> tooltip = itemStack.getTooltip(Item.TooltipContext.DEFAULT, MinecraftClient.getInstance().player, TooltipType.BASIC);
+		return tooltip.stream()
+				.anyMatch(text -> text.getString().toLowerCase().contains(searchString.toLowerCase()));
 	}
 }
