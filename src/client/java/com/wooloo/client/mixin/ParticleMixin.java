ceb8db9a6f2ce38825433fa28931a506ca439d51
brendon-pa <bpaolin1@binghamton.edu>
2024-10-23 00:49:57 -0400

    add auto update for options json file

--- src/client/java/com/wooloo/client/mixin/ParticleMixin.java
+++ src/client/java/com/wooloo/client/mixin/ParticleMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.ParticleS2CPacket;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,14 +18,18 @@ public class ParticleMixin {
 	@Inject(method = "onParticle(Lnet/minecraft/network/packet/s2c/play/ParticleS2CPacket;)V", at = @At("RETURN"))
 	public void afterOnParticle(ParticleS2CPacket packet, CallbackInfo ci) {
 		if(optionsInstance.getData().get("autowings").toString().equals("true")) {
-			if (packet.getParameters().getType() == ParticleTypes.INSTANT_EFFECT) {
+			if(packet.getParameters().getType() == ParticleTypes.WITCH) {
 				MinecraftClient client = MinecraftClient.getInstance();
 				if (client.player != null) {
 					if (client.player.getX() > 10000 && client.player.getX() < 11000 && client.player.getZ() > 3500 && client.player.getZ() < 4500) {
-						client.player.sendMessage(Text.literal("Coords: " + (int) packet.getX() + " " + (int) packet.getY() + " " + (int) packet.getZ()));
+						client.player.sendMessage(
+								Text.literal("Coords: " + (int) packet.getX() + " " + (int) packet.getY() + " " + (int) packet.getZ())
+								.withColor(Formatting.GRAY.getColorValue()));
 					}
 				}
 			}
 		}
 	}
 }
+
+// TODO: add raid detection (based on raid started/failed messages)
