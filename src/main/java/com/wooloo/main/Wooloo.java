d7cb24a23a4af73ff861b70493324cfb6b2c795a
brendon-pa <bpaolin1@binghamton.edu>
2024-11-30 23:52:35 -0500

    wanna test something...

--- src/main/java/com/wooloo/main/Wooloo.java
+++ src/main/java/com/wooloo/main/Wooloo.java
@@ -9,6 +9,7 @@ public class Wooloo implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+	// test.
     public static final Logger LOGGER = LoggerFactory.getLogger("wooloo");
 
 	@Override
